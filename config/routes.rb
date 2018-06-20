119315dba5763c17fb660c66d62365f00a46a22b
MozyOk <mozy.okubo@gmail.com>
2018-06-21 02:07:13 +0900

    change: Device Controller to User Controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'homes/index'
 
-  devise_for :users
+  devise_for :users, module: :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#index"
 end
