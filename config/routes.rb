ae9e5598dfa840f75979c911b687cffa3e206e4e
MozyOk <mozy.okubo@gmail.com>
2018-06-21 02:25:10 +0900

    add: error page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   devise_for :users, module: :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#index"
+
+  get '*path', controller: 'application', action: 'render_404'
+  
 end
